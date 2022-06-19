e59c173372f4770620916adf78c65b491f4466c3
kenng8891 <103011617+kenng8891@users.noreply.github.com>
2022-06-19 16:56:19 -0400

    lib js profiles completed

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,13 @@
+//call from employee.js
+const employee = require('../lib/employee')
+// const employee = new employee('kenny', 999, 'kenng8891')
+
+test('creates an employee', () => {
+    const employee = new employee('Kenny');
+    
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String))
+
+})
+
